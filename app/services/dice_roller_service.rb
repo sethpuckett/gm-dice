aca3f42e4e2751efe952fee29c3f654f89c00db8
Seth Puckett <puckett.seth@gmail.com>
2018-08-22 17:19:07 -0400

    dice roller logic

--- app/services/dice_roller_service.rb
+++ app/services/dice_roller_service.rb
@@ -4,6 +4,15 @@ class DiceRollerService
   class << self
     def roll(count: 1, sides: 6, constant: 0, attempts: 1)
       raise ArgumentError, 'Invalid Roll Input' unless input_valid?(count, sides, constant, attempts)
+
+      rolls = []
+
+      attempts.times do
+        rolls << calculate_attempt(count, sides, constant)
+      end
+
+      return rolls.first if rolls.length == 1
+      rolls
     end
 
     private
@@ -17,6 +26,14 @@ class DiceRollerService
 
     def valid_sides
       [4, 6, 8, 10, 12, 20, 100]
+    end
+
+    def calculate_attempt(count, sides, constant)
+      single_rolls = []
+      count.times do
+        single_rolls << rand(1..sides)
+      end
+      Roll.new(values: single_rolls, constant: constant)
     end
   end
 end
--- spec/services/dice_roller_service_spec.rb
+++ spec/services/dice_roller_service_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe DiceRollerService do
-  describe '.roll' do
+  describe '#roll' do
     context 'with invalid input' do
       it 'raises error with invalid sides' do
         expect { DiceRollerService.roll(sides: -1) }.to raise_error(ArgumentError)
@@ -23,6 +23,48 @@ RSpec.describe DiceRollerService do
       it 'raises error with invalid attempts' do
         expect { DiceRollerService.roll(attempts: 0) }.to raise_error(ArgumentError)
         expect { DiceRollerService.roll(attempts: 101) }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'with valid input' do
+      it 'returns singe roll with 1 attempt' do
+        roll = DiceRollerService.roll
+        expect(roll).to be_a(Roll)
+      end
+
+      it 'returns array of rolls with multiple attempts' do
+        rolls = DiceRollerService.roll(attempts: 3)
+        expect(rolls).to be_an(Array)
+        expect(rolls.length).to eq(3)
+        expect(rolls.first).to be_a(Roll)
+      end
+
+      it 'returns a non-zero total' do
+        roll = DiceRollerService.roll
+        expect(roll.total).to be > 0
+      end
+
+      it 'returns a total greater than constant' do
+        roll = DiceRollerService.roll(constant: 10)
+        expect(roll.total).to be > 10
+      end
+
+      it 'returns non zero total with all valid sides' do
+        roll4 = DiceRollerService.roll(sides: 4)
+        roll6 = DiceRollerService.roll(sides: 6)
+        roll8 = DiceRollerService.roll(sides: 8)
+        roll10 = DiceRollerService.roll(sides: 10)
+        roll12 = DiceRollerService.roll(sides: 12)
+        roll20 = DiceRollerService.roll(sides: 20)
+        roll100 = DiceRollerService.roll(sides: 100)
+
+        expect(roll4.total).to be > 0
+        expect(roll6.total).to be > 0
+        expect(roll8.total).to be > 0
+        expect(roll10.total).to be > 0
+        expect(roll12.total).to be > 0
+        expect(roll20.total).to be > 0
+        expect(roll100.total).to be > 0
       end
     end
   end
