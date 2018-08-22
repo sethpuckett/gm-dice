31efb17e7788a3d39f908d3897a7ab12d29f3309
Seth Puckett <puckett.seth@gmail.com>
2018-08-22 15:44:51 -0400

    add rubocop

--- app/controllers/dice_roller_controller.rb
+++ app/controllers/dice_roller_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class DiceRollerController < ApplicationController
   def roll
-    render json: {value: 5}, status: 200
+    render json: { value: 5 }, status: 200
   end
 end
--- app/services/dice_roller_service.rb
+++ app/services/dice_roller_service.rb
@@ -1,22 +1,23 @@
+# frozen_string_literal: true
+
 class DiceRollerService
-  def self.roll(count: 1, sides: 6, constant: 0, attempts: 1)
-    return 1 if input_valid?(count,sides,constant,attempts)
-    0
-  end
+  class << self
+    def roll(count: 1, sides: 6, constant: 0, attempts: 1)
+      return 1 if input_valid?(count, sides, constant, attempts)
+      0
+    end
 
-  private
+    private
 
-  def self.input_valid?(count, sides, constant, attempts)
-    count > 0 &&
-      count < 100 &&
-      constant > -1000000 &&
-      constant < 1000000 &&
-      attempts > 0 &&
-      attempts < 100 &&
-      valid_sides.include?(sides)
-  end
+    def input_valid?(count, sides, constant, attempts)
+      count.between?(1, 100) &&
+        constant.between?(-1_000_000, 1_000_000) &&
+        attempts.between?(1, 100) &&
+        valid_sides.include?(sides)
+    end
 
-  def self.valid_sides
-    [4, 6, 8, 10, 12, 20, 100]
+    def valid_sides
+      [4, 6, 8, 10, 12, 20, 100]
+    end
   end
 end
