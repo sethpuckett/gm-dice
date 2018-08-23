04a2451c8ecff332092502a75bd391ef985ac046
Seth Puckett <puckett.seth@gmail.com>
2018-08-23 18:19:17 -0400

    controller updates

--- app/controllers/dice_roller_controller.rb
+++ app/controllers/dice_roller_controller.rb
@@ -2,6 +2,34 @@
 
 class DiceRollerController < ApplicationController
   def roll
-    render json: { value: 5 }, status: 200
+    response = DiceRollerService.roll(roll_params)
+    render json: response, status: 200
+  end
+
+  private
+
+  def roll_params
+    p = params.permit(:count, :sides, :constant, :attempts).to_h.symbolize_keys
+    p[:count] = p[:count].to_i if p[:count]
+    p[:sides] = p[:sides].to_i if p[:sides]
+    p[:constant] = p[:constant].to_i if p[:constant]
+    p[:attempts] = p[:attempts].to_i if p[:attempts]
+    p
+  end
+
+  def count
+    params[:count]
+  end
+
+  def sides
+    params[:sides]
+  end
+
+  def constant
+    params[:constant]
+  end
+
+  def attempts
+    params[:attempts]
   end
 end
--- spec/controllers/dice_roller_controller_spec.rb
+++ spec/controllers/dice_roller_controller_spec.rb
@@ -4,14 +4,25 @@ require 'rails_helper'
 
 RSpec.describe DiceRollerController, type: :controller do
   describe 'GET /roll' do
-    subject { get :roll }
-
     it 'has json response type' do
+      get :roll
       expect(subject.content_type).to eq('application/json')
     end
 
     it 'has 200 response' do
+      get :roll
       expect(subject.status).to eq(200)
+    end
+
+    it 'calls dice roller service with no paramaters' do
+      expect(DiceRollerService).to receive(:roll)
+      get :roll
+    end
+
+    it 'calls dice roller service with parameters' do
+      expect(DiceRollerService).to receive(:roll).with(count: 2, sides: 8, constant: 1, attempts: 2).and_return({ })
+
+      get :roll, params: { count: 2, sides: 8, constant: 1, attempts: 2 }
     end
   end
 end
